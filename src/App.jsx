80e8029ced6ba9ced1f253d5500388bfaa2d8f43
BBeerBear <xiongx659@gmail.com>
2023-07-21 15:21:37 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div className='bg-black/25 w-screen h-screen p-10 flex flex-col gap-6 transition-all'>
+      <div className='font-zcool text-6xl text-sky-400 hover:text-7xl'>
+        祝熊玉琛生日快樂！
+      </div>
+      <div className='font-dancing text-6xl text-pink-400 hover:text-7xl'>
+        Happy Birthday!
+      </div>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        {/* <ReactAudioPlayer src='./happy_birthday.mp3' autoPlay loop /> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more Hello world it did't
-        work
-      </p>
-    </>
+    </div>
   );
 }
 
