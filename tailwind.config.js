2a654c564e129c9febdbdf2c3de21345ba1e5660
BBeerBear <xiongx659@gmail.com>
2023-07-21 15:25:05 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zcool: ['ZCOOL KuaiLe', 'sans-serif'],
+        dancing: ['Dancing Script', 'cursive'],
+      },
+      backgroundImage: {
+        mybg: "url('/src/assets/bg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
